54031511bd3ca0ebc5787749c81ef52abb122cae
Sagae <316190502+sagaeforge@users.noreply.github.com>
2022-08-15 12:00:00 +0900

    archive(linux-develop-test): VS 리눅스 원격 빌드 테스트

---
+++ linux-develop-test/Linux Develop Test/Linux Develop Test.cpp
@@ -0,0 +1,12 @@
+﻿// Linux Develop Test.cpp : 애플리케이션의 진입점을 정의합니다.
+//
+
+#include "Linux Develop Test.h"
+
+using namespace std;
+
+int main()
+{
+	std::cout << "Hello CMake. dasdasdad" << std::endl;
+	return 0;
+}
