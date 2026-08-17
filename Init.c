0c67dc6013d2f2e046fd228a508a82d1a0ca7bed
Sagae <316190502+sagaeforge@users.noreply.github.com>
2026-08-17 19:14:59 +0900

    snapshot(laptop): 작업트리 보존 — 옛날 프로젝트들 (뭔지 모르겠는 프로젝트들)/노트북 Project/NugungaUtility

--- Init.c
+++ Init.c
@@ -8,17 +8,16 @@
 void Init() { setlocale(LC_ALL, ""); }
 
 int main(int argc, char const *argv[]) {
+
   Init();
 
-  String *str1 = Create(String, StringConstructorCaster)(L"ABCDEF1");
-  String *str2 = Create(String, StringConstructorCaster)(L"ABCDEF1");
+  String *str1 = String("가나다");
+  String *str2 = String("가나다");
 
-  printf("%S\n", str1->Values);
-  printf("%u\n", str1->Length);
-  
+  printf("%S\n", str2->Values);
+  printf("%u\n", str2->Length);
+
   printf("%d\n", Operator2(str1, "==", str2, StrStrToInt));
-
-  
 
   GC_Clean();
   return 0;
--- Sources/String/String.c
+++ Sources/String/String.c
@@ -1,11 +1,11 @@
 
-#include <stdbool.h>
 #include "String.h"
 #include "Exception.h"
+#include <stdarg.h>
+#include <stdbool.h>
 
-static wchar_t *_StringValueCreate(Length length)
-{
-  return Create(wchar_t, NoConstructor)(sizeof(wchar_t) * length + 1);
+static wchar_t *_StringValueCreate(Length length) {
+  return Create(wchar_t, NoConstructor)(sizeof(wchar_t) * (length + 1));
 }
 
 static Length _String_Length(wchar_t *Datas) {
@@ -53,44 +53,86 @@ void String_Destructor(String **Obj) {
     (*Obj) = NULL;
 }
 
-String *String_Join(String *Obj1, String *Obj2)
-{
-  //TODO 해야함
+String *String_Join(String *Obj1, String *Obj2) {
+  // TODO 해야함
   printf("Join");
   return NULL;
 }
-void String_Append (String *Obj1, String *Obj2)
-{
-  //TODO 해야함
+void String_Append(String *Obj1, String *Obj2) {
+  // TODO 해야함
   return;
 }
-String *String_SubString (String *Obj1, String *Obj2)
-{
-  //TODO 해야함
+String *String_SubString(String *Obj1, String *Obj2) {
+  // TODO 해야함
   printf("subString");
   return NULL;
 }
-String *String_Loop (String *Obj1, Length Length)
-{
-  //TODO 해야함
+String *String_Loop(String *Obj1, Length Length) {
+  // TODO 해야함
   printf("Loop");
   return NULL;
 }
-StringAry *String_Split (String *Obj1, String *Obj2)
-{
-  //TODO 해야함
+StringAry *String_Split(String *Obj1, String *Obj2) {
+  // TODO 해야함
   printf("Split");
   return NULL;
 }
-
-bool String_Compare(String *Obj1, String *Obj2)
-{
-  if(Obj1->Length != Obj2->Length)
-    return false;
-  
+static bool _CountCheck(String *Obj, String *Find, int start) {
   int i;
-  for (size_t i = 0; i < Obj1->Length; i++)
-    if(Obj1->Values[i] != Obj2->Values[i])
+  for (i = 0; i < Find->Length; i++)
+    if (Obj->Values[start + i] != Find->Values[i])
       return false;
   return true;
+}
+bool String_Compare(String *Obj1, String *Obj2) {
+  if (Obj1->Length != Obj2->Length)
+    return false;
+
+  return _CountCheck(Obj1, Obj2, 0);
+}
+Length *String_Count(String *Obj, String *Find) {
+  if (Obj->Length < Find->Length)
+    return 0;
+
+  int count = 0;
+  int i;
+  for (i = 0; i < Obj->Length; i++)
+    if (Obj->Values[i] == Find->Values[0]) {
+      if (Obj->Length - i < Find->Length)
+        break;
+      if (_CountCheck(Obj, Find, i))
+        count++;
+    }
+  return count;
+}
+static int _CheckEmptyChs(int chs) {
+  return (chs == 32 && chs >= 9 && chs <= 13);
+}
+String *String_Trim(String *Obj) {
+  int i, start = 0, end = 0;
+  for (i = 0; i < Obj->Length; i++)
+    if (_CheckEmptyChs(Obj->Values[i]))
+      start++;
+    else
+      break;
+  for (i = Obj->Length - 1; i >= 0; i--)
+    if (_CheckEmptyChs(Obj->Values[i]))
+      end++;
+    else
+      break;
+
+  wchar_t *temp = _StringValueCreate(Obj->Length - (start + end));
+  for (i = 0; i < Obj->Length - end; i++)
+    temp[i] = Obj->Values[start + i];
+  temp[i] = '\0';
+
+  return String_Constructor(temp);
+}
+bool String_Contains(String *Obj, String *Find) {
+  return String_Count(Obj, Find) != 0;
+}
+wchar_t *String_Get(String *Obj) { return Obj->Values; }
+Length *String_GetLength(String *Obj) { return Obj->Length; }
+void String_Set(String *Obj1, String *Obj2) {
+  _String_Setting(Obj1, Obj2->Values);
 }
